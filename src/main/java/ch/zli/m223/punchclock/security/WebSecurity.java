d409e8ef07e0120f5990b4d9d5bd225458bf152b
martsa <martsalobsang@gmail.com>
2020-09-24 01:44:51 -0700

    able to access index file with minor chnages on permitALL() at websecurity.java

--- src/main/java/ch/zli/m223/punchclock/security/WebSecurity.java
+++ src/main/java/ch/zli/m223/punchclock/security/WebSecurity.java
@@ -31,7 +31,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().headers().frameOptions().disable().and().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-                .antMatchers(HttpMethod.GET,"/**/*.js","**/*.html","**/*.css").permitAll()
+                .antMatchers(HttpMethod.GET,"/**/*.js","**/*.html","**/*.css","/**").permitAll()
                 .antMatchers("/h2-console/**").permitAll()
 
                 .anyRequest().authenticated()
